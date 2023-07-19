d5cc6e368462dd4b6fcd6a3aea23c2b0a9945d24
Teruto Sato <211052@st.yohida-g.ac.jp>
2023-07-19 11:22:56 +0900

    とりあえず木だし直した

--- game.h
+++ game.h
@@ -55,13 +55,15 @@ private:
 	// 定数
 	//-------------------
 	static const int nMaxPlayer = 2;	//プレイヤーの最大数
-	static const int nMaxObject = 28;	//オブジェクトの最大数
+	static const int nMaxObject = 4;	//オブジェクトの最大数
+	static const int nMaxBG = 51;
 	static const float fPastPosX;		//過去のX座標
 
 	//-------------------
 	// プライベート関数
 	//-------------------
 	void CreateObj();	//オブジェクトの生成
+	void CreateWood();	//背景オブジェクト(木)の生成
 
 	//-------------------
 	// 静的メンバ変数
@@ -73,6 +75,7 @@ private:
 	static CSky*		m_pSky;						//空
 	static CMeshField*	m_pMeshField;				//メッシュフィールド
 	static CObjectX*	m_pObjectX[nMaxObject];		//オブジェクト
+	static CObjectX*	m_pObjBG[nMaxBG];			//背景オブジェクト(木)の生成
 	static CPlayer*		m_pPlayer[nMaxPlayer];		//プレイヤー
 };
 
